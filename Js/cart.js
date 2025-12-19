8ab24812d7306c35bd538bad797aadd0661df317
caalami123 <caalamiagency@gmail.com>
2025-12-19 23:10:58 +0300

    Delete Js directory

--- Js/cart.js
+++
@@ -1,6 +0,0 @@
-function addToCart(id) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push(id);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Added to cart");
-}
